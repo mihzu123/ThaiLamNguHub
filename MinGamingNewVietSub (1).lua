901f59910844708a1262a2811d7b8339c23879be
mihzu123 <157666423+mihzu123@users.noreply.github.com>
2024-02-06 21:36:35 +0700

    Add files via upload

--- MinGamingNewVietSub (1).lua
+++ MinGamingNewVietSub (1).lua
@@ -2677,34 +2677,6 @@ M:AddDropdown({
     end    
 })
 
-local SpeedAttackList = {"Attack Safe","0","0.1","0.15","0.175",}
-_G.SelectSpeedAttack = ""
-M:AddDropdown({
-    Name = "Select Speed Attack",
-    Default = "0,15",
-    Options = SpeedAttackList,
-    Flag = "AttackDelay",
-    Save = true,
-    Callback = function(Value)
-        _G.Attackdelay = Value
-    end    
-})
-
-DropdownDelayAttack:SetValue("Fast Attack")
-DropdownDelayAttack:OnChanged(function(Value)
-_G.AttackDelay = Value
-if _G.AttackDelay = "Attack Safe" then
-    _G.Fast_Delay = 0.10
-elseif _G.AttackDelay = "0" then
-    _G.Fast_Delay = 0
-elseif _G.AttackDelay = "0.1" then
-    _G.Fast_Delay = 0.1
-elseif _G.AttackDelay = "0.15" then
-    _G.Fast_Delay = 0.15
-elseif _G.AttackDelay = "0.175" then
-    _G.Fast_Delay = 0.175
-end
-end)
 
 M:AddToggle({
     Name = "Auto Haki Observation",
